bbd405f34af7575d31bba52ed482c6323da27ba1
Eric Blade <blade.eric@gmail.com>
2020-01-15 00:09:37 -0500

    Typescript Conversion / Interface adjustments (#67)
    * WIP
    
    * Breaking: Complete conversion to basic TypeScript usage
    
    - also processRequest now returns result: null instead of result: undefined
      when errors occur.
    - due to this, you may have to use
      const MWS = require('mws-simple').default
      instead of
      const MWS = require('mws-simple')
      if you are still using CJS require.

--- src/ServerError.ts
+++ src/ServerError.ts
@@ -1,8 +1,12 @@
-module.exports = class ServerError extends Error {
-    constructor(message, code, body) {
+export default class ServerError extends Error {
+    code = 0;
+
+    body = '';
+
+    constructor(message: string, code: number, body: string) {
         super(message);
         if (Error.captureStackTrace) Error.captureStackTrace(this, ServerError);
         this.code = code;
         this.body = body;
     }
-};
+}
--- src/getContentType.ts
+++ src/getContentType.ts
@@ -1,5 +1,5 @@
-module.exports = (content) => {
+export default (content: string) => {
     if (!content) return 'application/x-www-form-urlencoded; charset=utf-8';
-    if (content.slice(0,5) === '<?xml') return 'text/xml';
+    if (content.slice(0, 5) === '<?xml') return 'text/xml';
     return 'text/tab-separated-values; charset=iso-8859-1';
-}
+};
